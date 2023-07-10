2283ff31330a6b15c68302a978c0c23f7666c4e8
Gulfani Putra <gulfaniputra@gmail.com>
2023-07-10 19:44:29 +0800

    Add more Kalimantan provinces

--- assets/src/script.js
+++ assets/src/script.js
@@ -5,9 +5,11 @@ document.getElementById("enter").onclick = function () {
   let selectedOption = select.options[select.selectedIndex];
 
   if (selectedOption.text == "Pick A Province") {
-    document.getElementById("rate").innerHTML = "Please pick a province";
+    document.getElementById("rate").innerHTML = "Please pick a province!";
   } else if (selectedOption.text != "Pick A Province") {
     if (selectedOption.text == object.provinceName)
-      document.getElementById("rate").innerHTML = object.minWageRate;
+      document.getElementById(
+        "rate"
+      ).innerHTML = `The minimum wage rate: ${object.minWageRate}`;
   }
 };
