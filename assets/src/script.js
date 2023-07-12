3d9ebaf768766eda8aae14c3d8ab352f15cbd46e
Gulfani Putra <gulfaniputra@gmail.com>
2023-07-12 14:01:17 +0800

    Convert provinces from nested objects to arrays

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,15 +1,43 @@
-const json = '{"provinceName":"East Kalimantan", "minWageRate":2981379}';
-const object = JSON.parse(json);
+const provincesList = [
+  { provinceName: "West Kalimantan", minWageRate: 2399699 },
+  { provinceName: "Central Kalimantan", minWageRate: 2903145 },
+  { provinceName: "South Kalimantan", minWageRate: 2877449 },
+  { provinceName: "East Kalimantan", minWageRate: 2877449 },
+  { provinceName: "North Kalimantan", minWageRate: 3000804 },
+];
 
 document.getElementById("enter").onclick = function () {
   let selectedOption = select.options[select.selectedIndex];
 
-  if (selectedOption.text == "Pick A Province") {
+  if (selectedOption.text === "Pick A Province") {
     document.getElementById("rate").innerHTML = "Please pick a province!";
-  } else if (selectedOption.text != "Pick A Province") {
-    if (selectedOption.text == object.provinceName)
-      document.getElementById(
-        "rate"
-      ).innerHTML = `The minimum wage rate: ${object.minWageRate}`;
+  } else if (selectedOption.text !== "Pick A Province") {
+    switch (selectedOption.text) {
+      case provincesList[0].provinceName:
+        document.getElementById(
+          "rate"
+        ).innerHTML = `${provincesList[0].provinceName}'s minimum wage rate is ${provincesList[0].minWageRate}.`;
+        break;
+      case provincesList[1].provinceName:
+        document.getElementById(
+          "rate"
+        ).innerHTML = `${provincesList[1].provinceName}'s minimum wage rate is ${provincesList[1].minWageRate}.`;
+        break;
+      case provincesList[2].provinceName:
+        document.getElementById(
+          "rate"
+        ).innerHTML = `${provincesList[2].provinceName}'s minimum wage rate is ${provincesList[2].minWageRate}.`;
+        break;
+      case provincesList[3].provinceName:
+        document.getElementById(
+          "rate"
+        ).innerHTML = `${provincesList[3].provinceName}'s minimum wage rate is ${provincesList[3].minWageRate}.`;
+        break;
+      case provincesList[4].provinceName:
+        document.getElementById(
+          "rate"
+        ).innerHTML = `${provincesList[4].provinceName}'s minimum wage rate is ${provincesList[4].minWageRate}.`;
+        break;
+    }
   }
 };
