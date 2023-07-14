7f077bfd8032e2dfe4f4e266ce6ec708ce305fe2
Gulfani Putra <gulfaniputra@gmail.com>
2023-07-14 12:01:16 +0800

    Refactor from 'switch' back to 'if' statement

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,4 +1,5 @@
 const provincesList = [
+  { provinceName: "Pick A Province", minWageRate: 0 },
   { provinceName: "West Kalimantan", minWageRate: 2399699 },
   { provinceName: "Central Kalimantan", minWageRate: 2903145 },
   { provinceName: "South Kalimantan", minWageRate: 2877449 },
@@ -9,35 +10,11 @@ const provincesList = [
 document.getElementById("enter").onclick = function () {
   let selectedOption = select.options[select.selectedIndex];
 
-  if (selectedOption.text === "Pick A Province") {
-    document.getElementById("rate").innerHTML = "Please pick a province!";
-  } else if (selectedOption.text !== "Pick A Province") {
-    switch (selectedOption.text) {
-      case provincesList[0].provinceName:
-        document.getElementById(
-          "rate"
-        ).innerHTML = `${provincesList[0].provinceName}'s minimum wage rate is ${provincesList[0].minWageRate}.`;
-        break;
-      case provincesList[1].provinceName:
-        document.getElementById(
-          "rate"
-        ).innerHTML = `${provincesList[1].provinceName}'s minimum wage rate is ${provincesList[1].minWageRate}.`;
-        break;
-      case provincesList[2].provinceName:
-        document.getElementById(
-          "rate"
-        ).innerHTML = `${provincesList[2].provinceName}'s minimum wage rate is ${provincesList[2].minWageRate}.`;
-        break;
-      case provincesList[3].provinceName:
-        document.getElementById(
-          "rate"
-        ).innerHTML = `${provincesList[3].provinceName}'s minimum wage rate is ${provincesList[3].minWageRate}.`;
-        break;
-      case provincesList[4].provinceName:
-        document.getElementById(
-          "rate"
-        ).innerHTML = `${provincesList[4].provinceName}'s minimum wage rate is ${provincesList[4].minWageRate}.`;
-        break;
-    }
+  if (
+    selectedOption.text === provincesList[select.selectedIndex].provinceName
+  ) {
+    document.getElementById("rate").innerHTML = `The minimum wage rate is ${
+      provincesList[select.selectedIndex].minWageRate
+    }.`;
   }
 };
